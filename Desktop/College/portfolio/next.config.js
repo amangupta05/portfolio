b015ce45c67693912022bf9d16d07bb916de933f
amangupta05 <amangupta52001@gmail.com>
2025-01-14 06:30:13 -0500

    fixed error

--- Desktop/College/portfolio/next.config.js
+++ Desktop/College/portfolio/next.config.js
@@ -1,8 +1,11 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '/portfolio',
+  basePath: '/portfolio',  // Ensure this matches your GitHub repo name
   assetPrefix: '/portfolio/',
 }
+
+module.exports = nextConfig;
